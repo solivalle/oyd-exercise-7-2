3675f2ecd23a084ba0282a71cdb9949e9b1a73f7
solivalle <127651078+solivalle@users.noreply.github.com>
2026-06-07 10:03:09 -0600

    Trivial change

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,4 @@
+#trivial change to trigger terraform apply and destroy in the CI/CD pipeline
 resource "aws_sqs_queue" "main" {
   name                       = var.queue_name
   visibility_timeout_seconds = var.visibility_timeout_seconds
